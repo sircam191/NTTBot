7e0613085a40e6d8c2a48da47742d2d95ddbf8cf
sircam191 <sircam19@gmail.com>
2019-09-18 18:03:44 -0700

    Update SayCommand.java

--- src/main/java/mainStuff/SayCommand.java
+++ src/main/java/mainStuff/SayCommand.java
@@ -10,28 +10,29 @@ public class SayCommand extends ListenerAdapter {
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         String[] textInput = event.getMessage().getContentRaw().split("\\s+", 3);
         Role bigBois = event.getGuild().getRoleById("434983695288631297");
-        TextChannel sayChannel = event.getGuild().getTextChannelsByName(textInput[1],false).get(0);
-        //TODO Fix dis shit &
-        //if sayChannel = null give warning
+        Role regulars = event.getGuild().getRoleById("436654248906850304");
 
-        // Good practise to ignore bots.
+
+        //ignore bots.
         if(event.getAuthor().isBot()) {
             return;
         }
         if ((textInput[0].equalsIgnoreCase(Main.prefix + "say"))) {
-            if (event.getMember().getRoles().contains(bigBois) ) {
-
+            if (event.getMember().getRoles().contains(bigBois) || event.getMember().getRoles().contains(bigBois) ) {
                 try {
-                    sayChannel.sendMessage(textInput[2]).queue();
-                    //event.getGuild().getTextChannelsByName(textInput[1], true).get(0).sendMessage(textInput[2]).queue();
-
+                    TextChannel aChannel = event.getMessage().getMentionedChannels().get(0);
+                    String messageToSay = event.getMessage().getContentRaw().substring(4).replaceAll(aChannel.getAsMention(), "");
+                    aChannel.sendMessage(messageToSay).queue();
+                    event.getMessage().addReaction("\u2705").queue();
                }
                 catch (Exception e) {
                     event.getChannel().sendMessage("__ERROR__: Use this format: ``?say <Channel Name> <Text to say>``").queue();
+                    event.getMessage().addReaction("\u274C").queue();
                 }
             }
             else {
-                event.getChannel().sendMessage("__ERROR__: ``You must be a Big Boi to use this command``").queue();
+                event.getChannel().sendMessage("Sorry dogg only ``BIG BOIS`` and ``REGULARS`` can use this sick ass command...").queue();
+                event.getMessage().addReaction("\u274C").queue();
             }
         }
 
